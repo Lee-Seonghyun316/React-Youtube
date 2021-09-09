89b9d39f6ad634d5dac45b032a3052691de9fa9d
Lee-SeongHyun316 <mn0316@naver.com>
2021-09-09 12:03:51 +0900

    [Add] video title, thumnail connection

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import  styles from './video_item.module.css'
 const VideoItem = (props) => (
-    <h2>{props.video.snippet.title}</h2>
+    <li className={styles.video_item}>
+        <img  className={styles.video_thumnail} src={props.video.snippet.thumbnails.medium.url} alt="video thumnail"/>
+        <h2 className={styles.video_title}>{props.video.snippet.title}</h2>
+        <p className={styles.video_author}>{props.video.snippet.channelTitle}</p>
+    </li>
 );
 
 export default VideoItem;
